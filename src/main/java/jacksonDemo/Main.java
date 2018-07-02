7a7224965a4c445b80bd497bf22b6f8622ad10b3
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-02 09:20:13 +0200

    Jackson - prosty obiekt

---
+++ src/main/java/jacksonDemo/Main.java
@@ -0,0 +1,25 @@
+package jacksonDemo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try {
+			//utworzenie mapperra
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			//wczytanie pliku JSON i konwersja do POJO
+			Student student = objectMapper.readValue(new File("json/sample-lite.json"), Student.class);
+			
+			//wydruk pol
+			System.out.println(student);
+			
+		} catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
