d1a7e0cb3dccb13169899b7e9f2fc72f94519d24
Pl4cek <pl4cek@outlook.com>
2018-07-02 19:08:46 +0200

    Usuniety importy

--- src/main/java/com/nauka/spring5/rest/api/controller/StudentRestController.java
+++ src/main/java/com/nauka/spring5/rest/api/controller/StudentRestController.java
@@ -5,17 +5,12 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpServerErrorException;
 
 import com.nauka.spring5.rest.api.domain.Student;
-import com.nauka.spring5.rest.api.exceptions.StudentErrorResponse;
 import com.nauka.spring5.rest.api.exceptions.StudentNotFoundException;
 
 @RestController
